d734172c6ed8984b95f225a11d7e980c87e638f1
YUHASUNG <yuha822@naver.com>
2023-03-09 21:45:55 +0900

    dosc: Next.js 환경 변수

--- Next/my-app/pages/sub/fetch.js
+++ Next/my-app/pages/sub/fetch.js
@@ -1,16 +1,18 @@
-import { useEffect } from "react"
+import { useEffect,useState } from "react"
 
 export default function Fetch() {
+    const [user, setUser] = useState({name:null});
     useEffect(() => {
         // 접근이 가능한 환경변수 
         fetch(process.env.NEXT_PUBLIC_API_URL+'api/hello')
             .then(type => type.json())
             .then(result=> {
-                console.log(result);
+                setUser(result);
             })
     })
     return <>
         <h1>/pages/sub/Fetch.js</h1>
+        <p>{user.name}</p>
         <a href="/">/pages/index.js</a>
     </>
 }
