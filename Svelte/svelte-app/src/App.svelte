1e4fcd56f08cdd436e0d74541201228882912a9d
YUHASUNG <yuha822@naver.com>
2023-04-13 00:09:19 +0900

    dosc: svelte 기초

--- Svelte/svelte-app/src/App.svelte
+++ Svelte/svelte-app/src/App.svelte
@@ -1,9 +1,14 @@
 <script>
 	export let name;
+	// 반응형 state 변수
+	// let fullName = 'jung' + name; // 재랜더링이 안됨
+	$: fullName = 'Jung' + name; // $ 표기가 있으면 강제로 재랜더링이 가능
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<!-- <h1>Hello {name}!</h1> -->
+	<!-- state 변수  -->
+	<h1>Hello {fullName}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 	<!-- 버튼 기능 -->
 	<button on:click={() => {name = 'sung' }}>버튼</button> <!-- state 변경 -> name에서 sung으로! -->
