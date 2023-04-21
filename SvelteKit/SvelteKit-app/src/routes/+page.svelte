31422b35fb4661385fef2e4030d3de185ebb29b5
YUHASUNG <yuha822@naver.com>
2023-04-21 13:47:53 +0900

    dosc: SvelteKit Routing

--- SvelteKit/SvelteKit-app/src/routes/+page.svelte
+++ SvelteKit/SvelteKit-app/src/routes/+page.svelte
@@ -1,3 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
+<!-- <h1>Welcome to SvelteKit</h1>
 <h2>Hello world</h2>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
+
+<h1>Welcome home</h1>
+
+<a href="/about">About</a>
+<a href="/profile">Profile</a>
