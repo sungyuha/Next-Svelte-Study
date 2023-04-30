173b3e6b1cb2c2c9f63062e50f5487840ec7d6c5
YUHASUNG <yuha822@naver.com>
2023-05-01 00:33:36 +0900

    dosc: Catch All Routes

---
+++ SvelteKit/SvelteKit-app/src/routes/products/[...slug]/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { page } from '$app/stores';
+    console.log($page.params.slug);
+
+    //slug를 하면 모든 어떤 경로로 들어왔는지 알려줌
+    const slugArr = $page.props.slug.split('/');
+    console.log(slugArr);
+</script>
+
+<h1>Dosc 페이지</h1>
