512c86cda698e2b2230f6913849882d82137a774
YUHASUNG <yuha822@naver.com>
2023-05-01 19:39:52 +0900

    dosc: SvelteKit-Catch All Routes

--- SvelteKit/SvelteKit-app/src/routes/products/[...slug]/+page.svelte
+++ SvelteKit/SvelteKit-app/src/routes/products/[...slug]/+page.svelte
@@ -13,6 +13,7 @@
 <!-- 배열의 개수가 만약에 1개 라면 -->
 {#if slugArr.length === 1}
 <h1>Dosc 페이지 ; 주제 {slugArr[0]}</h1>
+<!-- 배열의 개수가 2개 라면 -->
 {:else if slugArr.length === 2}
 <h1>Dosc 페이지 ; 주제 {slugArr[0]} ; 내용 {slugArr[1]}</h1>
 {/if}
